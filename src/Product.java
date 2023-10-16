dc27c3eb7095073d44f1eb02cb1796170cb01b9f
OlgaTatcher <dai.akemi230318@gmail.com>
2023-10-16 13:01:23 +0300

    Created class Product

---
+++ src/Product.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+
+public class Product {
+    private String type;
+    private double price;
+    private boolean discount;
+    private LocalDate dateAdded;
+
+    public Product(String type, double price, boolean discount, LocalDate dateAdded) {
+        this.type = type;
+        this.price = price;
+        this.discount = discount;
+        this.dateAdded = dateAdded;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(boolean discount) {
+        this.discount = discount;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(LocalDate dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+}
