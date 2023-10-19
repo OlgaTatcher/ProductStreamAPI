30ab6d13310ae1f80d51d2812cdd20b17df8da22
OlgaTatcher <dai.akemi230318@gmail.com>
2023-10-19 14:44:30 +0300

    replacement from stream to array

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,57 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Stream<Product>products=Stream.of(new Product("Book",250.00,true,
-                LocalDate.of(2022, 03,01)),
-                new Product("Book",300.00,true,
+        List<Product>products=new ArrayList<>();
+        products.addAll(Arrays.asList(new Product("Book",250.00,true,
+                LocalDate.of(2022, 03,01)),new Product("Book",300.00,true,
                         LocalDate.of(2020,01,10)),
                 new Product("Book",350.00,false,
                         LocalDate.of(2021,01,20)),
                 new Product("Notebook",70.00,false,
                         LocalDate.of(2022,01,15)),
                 new Product("Notebook",55.00,true,
-                        LocalDate.of(2022,10,10)));
-        List<Product>book=products.filter(product -> "Book".equals(product.getType())&& (product.getPrice()>250))
+                        LocalDate.of(2022,10,10))));
+
+        List<Product>book=products.stream()
+                .filter(product -> "Book".equals(product.getType())&& (product.getPrice()>250))
                 .toList();
         book.forEach(b-> System.out.println(b.getType()+"-"+b.getPrice()));
 
-        Stream<Product>discountedProducts=Stream.of(new Product("Book",250.00,true,
-                        LocalDate.of(2022, 03,01)),
-                new Product("Book",300.00,true,
-                        LocalDate.of(2020,01,10)),
-                new Product("Book",350.00,false,
-                        LocalDate.of(2021,01,20)),
-                new Product("Notebook",70.00,false,
-                        LocalDate.of(2022,01,15)),
-                new Product("Notebook",55.00,true,
-                        LocalDate.of(2022,10,10)));
-        discountedProducts.filter(product -> "Book".equals(product.getType()))
-                .flatMap(p1->Stream.of(
-                        String.format("Price without discount:"+p1.getType()+" "+p1.getPrice()),
-                        String.format("Discounted price: "+p1.getType()+" "+(p1.getPrice()-(int)(p1.getPrice()*0.1)))
-                ))
-                .forEach(pwd-> System.out.println(pwd));
+
+        List<Product>discountedProducts=products.stream()
+                .filter(product -> "Book".equals(product.getType()))
+                .map(product -> new Product(product.getType(), product.getPrice(), product.withDiscount(), product.getDateAdded()))
+                .toList();
+        discountedProducts.forEach(dp-> System.out.println("Discounted Products: "+dp.getType()+" -price with discount: "
+                +(dp.getPrice() * 0.9)));
 
 
-        Stream<Product>cheapestProduct=Stream.of(new Product("Book",250.00,true,
-                        LocalDate.of(2022, 03,01)),
-                new Product("Book",300.00,true,
-                        LocalDate.of(2020,01,10)),
-                new Product("Book",350.00,false,
-                        LocalDate.of(2021,01,20)),
-                new Product("Notebook",70.00,false,
-                        LocalDate.of(2022,01,15)),
-                new Product("Notebook",55.00,true,
-                        LocalDate.of(2022,10,10)));
+        Optional<Product>cheapestProduct=products.stream()
+                .filter(product -> "Book".equals(product.getType()))
+                .min(Product::compare);
+        cheapestProduct.ifPresentOrElse(
+                cp-> System.out.println("Cheapest "+cp.getType()+" costs "+cp.getPrice()),
+                ()-> System.out.println("Product not found")
+        );
 
-        Product min = cheapestProduct.filter(product -> "Book".equals(product.getType()))
-                .min(Product::compare).get();
-        System.out.println("Cheapest product:type- "+min.getType()+"- price-"+min.getPrice());
+        List<Product>lastThreeAddedProducts=products.stream()
+                .sorted(Comparator.comparing(Product::getDateAdded).reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+        lastThreeAddedProducts.forEach(lap-> System.out.println(lap.getDateAdded()));
+
+
+
+
+
+
+
+
+
 
 
 
